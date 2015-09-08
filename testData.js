c939c36972b05dff7dce73782e48574695b8131b
Adam <adam@ebusiness-solutions.ca>
2015-09-08 13:23:20 -0400

    Initial Drop of Project

---
+++ testData.js
@@ -0,0 +1,37 @@
+
+// The data model with templates --> projects --> inspections --> photos
+
+var templates = [{templateName: 'Inspection Report', projects:
+    [{projectName: 'Project1', clientName: 'Client1', inspections:
+        [{inspectionNumber: 1, inspectorName: 'Ins1', photos:[]},
+            {inspectionNumber: 2, inspectorName: 'Ins2',photos:[]},
+            {inspectionNumber: 3, inspectorName: 'Ins3',photos:[]}]},
+        {projectName: 'Project2', clientName: 'Client2', inspections:
+            [{inspectionNumber: 1, inspectorName: 'Ins2-1',photos:[]},
+                {inspectionNumber: 2, inspectorName: 'Ins2-2',photos:[]},
+                {inspectionNumber: 3, inspectorName: 'Ins2-3',photos:[]}]}
+    ]}];
+templates[0].projects[0].inspections[0].photos = [
+    {photoNumber: 1, fileName:"Photo1.jpg"},
+    {photoNumber: 2, fileName:"Photo2.jpg"}];
+templates[0].projects[0].inspections[1].photos = [
+    {photoNumber: 1, fileName:"Photo3.jpg"},
+    {photoNumber: 2, fileName:"Photo4.jpg"}];
+
+// Active row numbers
+var iTemplate = 0;
+var iProject = 0;
+var iInspection = 0;
+var iPhoto = 0;
+
+// Pointers for active lists
+var projects = templates[iTemplate].projects;
+var inspections = projects[iProject].inspections;
+var photos = inspections[iInspection].photos;
+
+// Pointers for active forms
+var template = templates[iTemplate];
+var project = projects[iProject];
+var inspection = inspections[iInspection];
+var photo = photos[iPhoto];
+
